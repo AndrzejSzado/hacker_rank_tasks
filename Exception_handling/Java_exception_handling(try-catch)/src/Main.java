7c173006074db1cfdfd661bcff8b8857b0919eea
AndrzejSzado <31636650+AndrzejSzado@users.noreply.github.com>
2018-08-01 19:42:24 +0200

    Exception handling tasks sloved

--- Exception_handling/Java_exception_handling(try-catch)/src/Main.java
+++ Exception_handling/Java_exception_handling(try-catch)/src/Main.java
@@ -1,6 +1,19 @@
-public class Main {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner reader = new Scanner(System.in);
+        try {
+            int x = reader.nextInt();
+            int y = reader.nextInt();
+            System.out.println(x/y);
+        }
+        catch (InputMismatchException e){
+            System.out.println(e.getClass().getName());
+        }
+        catch (ArithmeticException e) {
+            System.out.println(e.getClass().getName() + ": / by zero");
+        }
     }
 }
